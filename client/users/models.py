84c5537d57f06fe9aef0e37c60c6b8288a75a9d9
Aaron Mekonnen <meko0019@umn.edu>
2019-05-11 21:37:07 -0500

    tx

--- client/users/models.py
+++ client/users/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import validates
 from client.database import db, BaseModel, UTCNOW, isofmt_date
 
 class User(BaseModel):
+	__tablename__ = 'users'
 	username = db.Column(db.String(64), index=True, nullable=False, unique=True)
 	email_address = db.Column(db.String(120), index=True, nullable=True, unique=True)
 	first_name = db.Column(db.String(64), index=True, nullable=False)
